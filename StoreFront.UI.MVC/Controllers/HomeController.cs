3045868aeb756b08e1f0af7281543ec30ef39cef
Corbin Brown <cjbrown019@gmail.com>
2023-02-20 18:53:00 -0600

    Converted template

--- StoreFront.UI.MVC/Controllers/HomeController.cs
+++ StoreFront.UI.MVC/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace StoreFront.UI.MVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Item()
+        {
+            return View();
+        }
     }
 }
